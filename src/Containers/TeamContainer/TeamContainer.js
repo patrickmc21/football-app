8ad6b33b30cedb4472ad6fb12ad3032c18d80552
Patrick McLaughlin <patrickmc21@gmail.com>
2018-08-26 14:20:42 -0600

    Add add to team action, stub out team container

--- src/Containers/TeamContainer/TeamContainer.js
+++ src/Containers/TeamContainer/TeamContainer.js
@@ -5,9 +5,63 @@ import './TeamContainer.css';
 
 class TeamContainer extends Component {
   render() {
+    const { team } = this.props;
     return (
       <section>
-        Team Container
+        <article>
+          <div>
+            <ul>
+              <li>
+                QB: {team.QB || "empty"}
+              </li>
+              <li>
+                RB: {team.RB1 || "empty"}
+              </li>
+              <li>
+                RB: {team.RB2 || "empty"}
+              </li>
+              <li>
+                WR: {team.WR2 || "empty"}
+              </li>
+              <li>
+                WR: {team.WR2 || "empty"}
+              </li>
+              <li>
+                TE: {team.TE || "empty"}
+              </li>
+              <li>
+                FLEX: {team.FLEX || "empty"}
+              </li>
+              <li>
+                DST: {team.DST || "empty"}
+              </li>
+              <li>
+                K: {team.K || "empty"}
+              </li>
+              <li>
+                BE: {team.BE1 || "empty"}
+              </li>
+              <li>
+                BE: {team.BE2 || "empty"}
+              </li>
+              <li>
+                BE: {team.BE3 || "empty"}
+              </li>
+              <li>
+                BE: {team.BE4 || "empty"}
+              </li>
+              <li>
+                BE: {team.BE5 || "empty"}
+              </li>
+              <li>
+                BE: {team.BE6 || "empty"}
+              </li>
+              <li>
+                BE: {team.BE7 || "empty"}
+              </li>
+            </ul>
+          </div>
+        </article>
       </section>
     )
   }
--- src/Reducers/team.js
+++ src/Reducers/team.js
@@ -19,6 +19,97 @@ const teamModel = {
 
 const teamReducer = (state = teamModel, action) => {
   switch (action.type) {
+    case 'ADD_PLAYER_TO_TEAM':
+      const { player } = action;
+      const benchArray = ['BE1', 'BE2', 'BE3', 'BE4', 'BE5', 'BE6', 'BE7'];
+      const newState = {...state};
+      switch (player.position) {
+        case 'QB':
+          if (!newState.QB) {
+              newState.QB = player.name;
+            } else {
+              for (let i = 0; i < benchArray.length; i++) {
+                if (!newState[benchArray[i]]) {
+                  newState[benchArray[i]] = player.name;
+                  i = benchArray.length;
+                }
+              }; 
+            }
+            break;
+        case 'RB':
+          if (!newState.RB1) {
+              newState.RB1 = player.name;
+            } else if (!newState.RB2) {
+              newState.RB2 = player.name;
+            } else if (!newState.FLEX) {
+              newState.FLEX = player.name;
+            } else {
+              for (let i = 0; i < benchArray.length; i++) {
+                if (!newState[benchArray[i]]) {
+                  newState[benchArray[i]] = player.name;
+                  i = benchArray.length;
+                }
+              }; 
+            }
+            break;
+        case 'WR':
+          if (!newState.WR1) {
+              newState.WR1 = player.name;
+            } else if (!newState.WR2) {
+              newState.WR2 = player.name;
+            } else if (!newState.FLEX) {
+              newState.FLEX = player.name;
+            } else {
+              for (let i = 0; i < benchArray.length; i++) {
+                if (!newState[benchArray[i]]) {
+                  newState[benchArray[i]] = player.name;
+                  i = benchArray.length;
+                }
+              }; 
+            }
+            break;
+        case 'TE':
+          if (!newState.TE) {
+              newState.TE = player.name;
+            } else if (!newState.FLEX) {
+              newState.FLEX = player.name;
+            } else {
+              for (let i = 0; i < benchArray.length; i++) {
+                if (!newState[benchArray[i]]) {
+                  newState[benchArray[i]] = player.name;
+                  i = benchArray.length;
+                }
+              }; 
+            }
+            break;
+        case 'DST':
+          if (!newState.DST) {
+              newState.DST = player.name;
+            } else {
+              for (let i = 0; i < benchArray.length; i++) {
+                if (!newState[benchArray[i]]) {
+                  newState[benchArray[i]] = player.name;
+                  i = benchArray.length;
+                }
+              }; 
+            }
+            break;
+        case 'K':
+          if (!newState.K) {
+            newState.K = player.name;
+          } else {
+            for (let i = 0; i < benchArray.length; i++) {
+              if (!newState[benchArray[i]]) {
+                newState[benchArray[i]] = player.name;
+                i = benchArray.length;
+              }
+            }; 
+          }
+          break;
+        default:
+          break;
+      }
+      return newState;
     default:
       return state;
   }
